06e8ffcb6ab5762a7af21f957b67157112561287
7373768329392848473u3636363663737273736 <99855568+7373768329392848473u3636363663737273736@users.noreply.github.com>
2022-08-12 18:31:59 +0500

    Add files via upload

---
+++ socks5.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* U KNOW LAH HEHEHE :V 
+* by Angga Kurniawan (g4rzk)
+*/
+
+    $url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&country=all";
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $replace = preg_replace("/\s+/", "<br>", $output);
+    echo $replace;
+
+?>
